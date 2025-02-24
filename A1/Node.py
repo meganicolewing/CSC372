2cf85cc7d735489822d7737e09d1fa3fe479afd2
megan <megan.ewing@centre.edu>
2025-02-24 10:40:54 -0500

    added search class with all search methods

--- A1/Node.py
+++ A1/Node.py
@@ -1,10 +1,11 @@
 class Node:
 
-    def __init__(self, parent, action:int, state, path_cost:int, heuristic:float):
+    def __init__(self, parent, action:int, state, path_cost:int, heuristic:float=0):
         '''initializes a node with all necesary properties, where the parent is a Node and the state is a Cube.
-        the action is an int that indicates the rotation that, when performed on the parent's state, results in this state.'''
-        self.parent
-        self.action
-        self.state
-        self.path_cost
-        self.heuristic
+        the action is an int that indicates the rotation that, when performed on the parent's state, results in this state.
+        a heuristic value may be given for A* search. otherwise, the value defaults to 0.'''
+        self.parent = parent
+        self.action = action
+        self.state = state
+        self.path_cost = path_cost
+        self.total_cost = path_cost + heuristic
---
+++ A1/Search.py
@@ -0,0 +1,23 @@
+from queue import PriorityQueue
+import Node
+class Search:
+
+    @staticmethod
+    def breadth_first(cube):
+        pass
+
+    @staticmethod
+    def iterative_deepening(cube):
+        pass
+
+    @staticmethod
+    def deepening_A_star(cube):
+        pass
+
+    @staticmethod
+    def __depth_first(cube, max_depth:int):
+        pass
+
+    @staticmethod
+    def __A_star(cube,max_depth:int):
+        pass
