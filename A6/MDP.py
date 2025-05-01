f5761e4bb64c62cf37efbfcf6ec7b2f0c03cb75f
megan <megan.ewing@centre.edu>
2025-05-01 17:14:30 -0400

    implemented value iteration

--- A6/MDP.py
+++ A6/MDP.py
@@ -54,3 +54,41 @@ class MDP:
             if(next_state == (current_state-1) or (((current_state)%4)==0 and (current_state+1)==next_state)):
                 retval = retval + 0.2
         return retval
+
+    #calculates the utility of every state, within the given error
+    def utilityFuction(self,error=float)->list[float]:
+        utility = []
+        new_utility = []
+        for i in self.states:
+            new_utility.append(0)
+            utility.append(0)
+        delta = error * (1.0-self.discount)/self.discount
+        while delta>=(error * (1.0-self.discount)/self.discount):
+            for i in range(len(self.states)):
+                utility[i] = new_utility[i]
+            delta = 0
+            for current in self.states:
+                sumOverActions = []
+                for action in self.actions:
+                    sumOverActions.append(0)
+                    for next in self.states:
+                        sumOverActions[action] = sumOverActions[action] + self.transition_matrix[next][current][action]*utility[next]
+                new_utility[current] = self.rewards[current] + self.discount*max(sumOverActions)
+                delta = max(delta,abs(utility[current],new_utility[current]))
+        return utility
+
+    #determines the optimal policy using value iteration
+    #given the error for the utility
+    def valueIteration(self,error=float)->tuple[list[int],list[float]]:
+        utility = self.utilityFuction(error)
+        policy = []
+        for current in self.states:
+            policy.append[0]
+            valueOfActions = []
+            for a in self.actions:
+                valueOfActions.append(0)
+                for next in self.states:
+                    valueOfActions[a] = valueOfActions[a] + self.transition_matrix[next][current][a]*utility[next]
+            max_util = max(valueOfActions)
+            policy[current]=valueOfActions.index(max_util)
+        return policy,utility
